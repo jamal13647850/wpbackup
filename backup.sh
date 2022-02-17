a777866007f557f92205e665fc2afa41dfe7f888
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-02-17 13:47:34 +0330

    Some changes

--- backup.sh
+++ backup.sh
@@ -2,13 +2,15 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 DIR=`date +"%Y%m%d-%H%M%S"`
 DEST=$SCRIPTPATH/backups/$DIR
 DBZIP='DB-'$DIR'.zip'
-FILESZIP='Files-'$DIR'.zip'
+BACKUP_RETAIN_DURATION=10
 
 
 mkdir -pv $SCRIPTPATH/backups
+find $SCRIPTPATH/backups/ -mtime +"${BACKUP_RETAIN_DURATION}" -exec rm -rfv {} \;
+
 mkdir -pv $DEST
 mkdir -pv $DEST/DB
-mkdir -pv $DEST/Files
+
 
 cd $DEST/DB
 wp db export --add-drop-table --path=/var/www/html
@@ -16,16 +18,14 @@ cd $DEST
 zip -r9 $DBZIP DB/
 rm -rfv DB/
 
-
-cp -rfv /var/www/html/* $DEST/Files
-cd $DEST
-zip -r9 $FILESZIP Files/
-rm -rfv Files/
-
 #ssh-keygen
 #ssh-copy-id -i ~/.ssh/id_rsa.pub -p port [destuser]@[destip]
 
-rsync -azvrh --progress $DEST/$DBZIP -e 'ssh -p [destport] -i /home/[srcuser]/.ssh/id_rsa' [destuser]@[destip]:[destbackuppath]
-rsync -azvrh --progress $DEST/$FILESZIP -e 'ssh -p [destport] -i /home/[srcuser]/.ssh/id_rsa' [destuser]@[destip]:[destbackuppath]
+rsync -azvrh --progress $DEST/$DBZIP -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destdbbackuppath]
+rsync -azvrh --progress /var/www/html/** -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destfilesbackuppath]
 
-#0 0 3 * * sh /path/to/backup.sh >>  /path/to/backup.log 2>&1
+
+
+#0 21 * * * sh /path/to/backup.sh >>  /path/to/backup.log 2>&1
+
+#0 21 * * * sh /var/www/backups/backup.sh >>  /var/www/backups/backup.log 2>&1
