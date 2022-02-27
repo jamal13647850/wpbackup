6d9f4e4e8941765ef37acc548e2a83c222a25db4
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2022-02-28 01:10:22 +0330

    Add some exclude

--- backup.sh
+++ backup.sh
@@ -21,6 +21,6 @@ rm -rfv DB/
 
 
 rsync -azvrh --progress $DEST/$DBZIP -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destdbbackuppath]
-rsync -azvrh --progress --exclude 'fm' --exclude 'nginx.conf' /var/www/html/** -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destfilesbackuppath]
+rsync -azvrh --progress --exclude 'fm' --exclude 'wp-content/cache' --exclude 'wp-content/debug.log' --exclude 'wp-content/backups-dup-pro' --exclude 'nginx.conf' /var/www/html/** -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destfilesbackuppath]
 
 
--- files.sh
+++ files.sh
@@ -1,6 +1,6 @@
 
 
-rsync -azvrh --progress --exclude 'fm' --exclude 'nginx.conf' /var/www/html/** -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destfilesbackuppath]
+rsync -azvrh --progress --exclude 'fm' --exclude 'wp-content/cache' --exclude 'wp-content/debug.log' --exclude 'wp-content/backups-dup-pro' --exclude 'nginx.conf' /var/www/html/** -e 'ssh -p [destport] -i /root/.ssh/id_rsa' [destuser]@[destip]:[destfilesbackuppath]
 
 
 
